3256d228e3f73643d79ec9ced7711fff493eebff
Arisa <angelevarela@hotmail.com>
2023-01-09 09:09:49 -0800

    Changed the code to work completely and refactored a bit

--- webpToJPEG.py
+++ webpToJPEG.py
@@ -1,13 +1,10 @@
 from PIL import Image
 import os
-
 directory = os.getcwd()
 
 for filename in os.listdir(directory):
     if filename.endswith(".webp"):
         im = Image.open(filename)
         filename_without_ext = os.path.splitext(filename)[0]
-        im.save(filename_without_ext, "jpeg")
-        continue
-    else:
-        continue
+        im.save(filename_without_ext + ".jpeg", "png")
+    
